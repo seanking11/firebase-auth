62c9f1d9d8302f11abbb3d8c2734b187ed5413f1
Sean King <kingsc@miamioh.edu>
2017-12-30 18:27:05 -0600

    feat(GCF): Add one-time-password directory
    To be used as reference for the Google Cloud Functions

---
+++ one-time-password/functions/index.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const serviceAccount = require('./service_account.json')
+const createUser = require('./create_user')
+const requestOneTimePassword = require('./request_one_time_password')
+const verifyOneTimePassword = require('./verify_one_time_password')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://rn-swipe.firebaseio.com'
+})
+
+exports.createUser = functions.https.onRequest(createUser)
+exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword)
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword)
---
+++ one-time-password/functions/twilioKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  accountSid: 'AC8ebb3a80a4ca2c584bc72908c0010e7a',
+  authToken: '358da3a5e900920562cf10bd27aaa3e3'
+}
